be904df0e17de971c8766dc9f46ef6151b410076
affannadir680 <affan@codtronix.com>
2026-02-13 00:58:43 +0500

    Delete tests/loginchecker.spec.js

--- tests/loginchecker.spec.js
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-import LoginPage from '../pageObjects/logincheck.js';
-
-test('User can log in with valid credentials', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await page.goto('https://www.saucedemo.com/');
-    await loginPage.login('standard_user', 'secret_sauce');
-});
